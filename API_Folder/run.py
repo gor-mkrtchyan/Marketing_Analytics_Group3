fa77be07bfc2bdb532ae0de3f99f1a2c06276232
manedavtyan <mane_davtyan@edu.aua.am>
2023-11-06 21:24:05 +0400

    API Folder adjustments

--- API_Folder/run.py
+++ API_Folder/run.py
@@ -2,4 +2,5 @@ import uvicorn
 from api.api import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
