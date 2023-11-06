bfae732f0992ddd8864c98cf9fe464634911ab90
manedavtyan <mane_davtyan@edu.aua.am>
2023-11-06 21:05:29 +0400

    API Folder adjustments

--- API_Folder/api/api.py
+++ API_Folder/api/api.py
@@ -1,20 +1,64 @@
 import pandas as pd
-import numpy as np
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-#Read the data
+app = FastAPI()
 
-data=pd.read_csv('API_Folder/authors.csv')
+# Load data
+books_data = pd.read_csv("api/data/books.csv")
+customers_data = pd.read_csv("api/data/customers.csv")
 
-#create instance called app
-app=FastAPI()
-
-@app.get('/')
+@app.get("/")
 def read_root():
-    return{"message":"Hello, World"}
+    return {"message": "BookStore API"}
 
-@app.get("/get_info/")
-def get_info(ID:int):
-    """THis code is to get the id of a person from the data """
-    info=data[data.id==ID].to_dict('records')
-    return (info)
+@app.get("/books/")
+def get_books():
+    return books_data.to_dict(orient="records")
+
+@app.get("/customers/")
+def get_customers():
+    return customers_data.to_dict(orient="records")
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    book = books_data[books_data["book_id"] == book_id]
+    if book.empty:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book.to_dict(orient="records")[0]
+
+@app.get("/customers/{customer_id}")
+def get_customer(customer_id: int):
+    customer = customers_data[customers_data["customer_id"] == customer_id]
+    if customer.empty:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return customer.to_dict(orient="records")[0]
+
+@app.post("/books/")
+def create_book(book: dict):
+    books_data = books_data.append(book, ignore_index=True)
+    books_data.to_csv("api/data/books.csv", index=False)
+    return book
+
+@app.post("/customers/")
+def create_customer(customer: dict):
+    customers_data = customers_data.append(customer, ignore_index=True)
+    customers_data.to_csv("api/data/customers.csv", index=False)
+    return customer
+
+@app.put("/books/{book_id}")
+def update_book(book_id: int, book: dict):
+    book_index = books_data.index[books_data["book_id"] == book_id]
+    if book_index.empty:
+        raise HTTPException(status_code=404, detail="Book not found")
+    books_data.loc[book_index] = book
+    books_data.to_csv("api/data/books.csv", index=False)
+    return book
+
+@app.put("/customers/{customer_id}")
+def update_customer(customer_id: int, customer: dict):
+    customer_index = customers_data.index[customers_data["customer_id"] == customer_id]
+    if customer_index.empty:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    customers_data.loc[customer_index] = customer
+    customers_data.to_csv("api/data/customers.csv", index=False)
+    return customer
--- API_Folder/run.py
+++ API_Folder/run.py
@@ -1,7 +1,5 @@
 import uvicorn
-import os
 from api.api import app
 
-
-if __name__=="__main__":
-    uvicorn.run(app)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
