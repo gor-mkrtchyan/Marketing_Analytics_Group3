7ddd098b6b80255d1fd61f969768e3ac9672c3f8
manedavtyan <mane_davtyan@edu.aua.am>
2023-11-10 12:08:46 +0400

    API Folder adjustments

--- API_Folder/api/api.py
+++ API_Folder/api/api.py
@@ -1,11 +1,22 @@
 import pandas as pd
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 # Load data
 books_data = pd.read_csv("api/data/books.csv")
-customers_data = pd.read_csv("api/data/customers.csv")
+
+class Book(BaseModel):
+    title: Optional[str] = None
+    price: Optional[float] = None
+    isbn: Optional[str] = None
+    publication_year: Optional[int] = None
+    language: Optional[str] = None
+    cover_type: Optional[str] = None
+    pages_number: Optional[int] = None
+    book_id: int
 
 @app.get("/")
 def read_root():
@@ -15,10 +26,6 @@ def read_root():
 def get_books():
     return books_data.to_dict(orient="records")
 
-@app.get("/customers/")
-def get_customers():
-    return customers_data.to_dict(orient="records")
-
 @app.get("/books/{book_id}")
 def get_book(book_id: int):
     book = books_data[books_data["book_id"] == book_id]
@@ -26,39 +33,27 @@ def get_book(book_id: int):
         raise HTTPException(status_code=404, detail="Book not found")
     return book.to_dict(orient="records")[0]
 
-@app.get("/customers/{customer_id}")
-def get_customer(customer_id: int):
-    customer = customers_data[customers_data["customer_id"] == customer_id]
-    if customer.empty:
-        raise HTTPException(status_code=404, detail="Customer not found")
-    return customer.to_dict(orient="records")[0]
-
-@app.post("/books/")
-def create_book(book: dict):
-    books_data = books_data.append(book, ignore_index=True)
-    books_data.to_csv("api/data/books.csv", index=False)
-    return book
-
-@app.post("/customers/")
-def create_customer(customer: dict):
-    customers_data = customers_data.append(customer, ignore_index=True)
-    customers_data.to_csv("api/data/customers.csv", index=False)
-    return customer
-
 @app.put("/books/{book_id}")
-def update_book(book_id: int, book: dict):
+def update_book(book_id: int, book: Book):
     book_index = books_data.index[books_data["book_id"] == book_id]
     if book_index.empty:
         raise HTTPException(status_code=404, detail="Book not found")
-    books_data.loc[book_index] = book
+
+    book_data = books_data.loc[book_index]
+    for field, value in book.dict(exclude_unset=True).items():
+        if field != "book_id" and value is not None:
+            book_data[field] = value
+
+    books_data.loc[book_index] = book_data
+    books_data.to_csv("api/data/books.csv", index=False)
+    return {"book_id": book_id, **book_data.to_dict(orient="records")[0]}
+
+
+@app.post("/books/", response_model=Book)
+def create_book(book: Book):
+    new_book_id = books_data["book_id"].max() + 1
+    book_dict = book.dict()
+    book_dict["book_id"] = new_book_id
+    books_data = books_data.append(book_dict, ignore_index=True)
     books_data.to_csv("api/data/books.csv", index=False)
     return book
-
-@app.put("/customers/{customer_id}")
-def update_customer(customer_id: int, customer: dict):
-    customer_index = customers_data.index[customers_data["customer_id"] == customer_id]
-    if customer_index.empty:
-        raise HTTPException(status_code=404, detail="Customer not found")
-    customers_data.loc[customer_index] = customer
-    customers_data.to_csv("api/data/customers.csv", index=False)
-    return customer
